a409101c5e7054f3aa5ea61f27f2ea2bc14700ee
datata <dani@danitarazona.dev>
2024-04-23 12:36:47 +0200

    feat: get all tasks with model

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,13 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Task;
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
     public function getAllTasks()
     {
-        return 'GET ALL TASKS';
+        try {
+            $tasks = Task::all();
+
+            return response()->json(
+                [
+                    "success" => true,
+                    "message" => "Tasks retrieved successfully",
+                    "data" => $tasks        
+                ],
+                200
+            );
+        } catch (\Throwable $th) {
+            return response()->json(
+                [
+                    "success" => false,
+                    "message" => "Tasks cant be retrieved successfully",
+                    "error" => $th->getMessage()
+                ],
+                500
+            );
+        }
     }
 
     public function createTask(Request $request)
